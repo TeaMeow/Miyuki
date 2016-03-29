96924f630408f0efc5a17a2cd05c920983d3b5c8
Yami Odymel <yamiodymel@gmail.com>
2016-03-29 18:18:01 +0000

    Allowed the original path with write(true), and added setImageCompression().

--- src/miyuki.php
+++ src/miyuki.php
@@ -4,81 +4,81 @@ class Miyuki
 {
     public $filter  = Imagick::FILTER_POINT;
     public $quality = 80;
-    
-    
+
+
     function __construct($imagePath = null)
     {
         if($imagePath !== null)
             $this->create($imagePath);
     }
-    
-    
-    
-    
+
+
+
+
     /**
      * Create
-     * 
+     *
      * Create an image block for later to use.
-     * 
+     *
      * @param string $imagePath   The path of the image which we are going to process with.
-     * 
+     *
      * @return Miyuki
      */
-    
+
     function create($imagePath)
     {
         $this->imageBlock = new imagick($imagePath);
         $this->imagePath  = $imagePath;
         $this->imageType  = $this->getType();
         $this->imageSize  = $this->getSize();
-        
+
         return $this;
     }
-    
-    
-    
-    
+
+
+
+
     /**
      * Crop
      *
      * Crop the image to a target size.
-     * 
+     *
      * @param int $width    The width of the cropped image.
      * @param int $height   The height of the cropped image.
      * @param int $x        The x start point of the uncropped image.
      * @paeam int $y        The y start point of the uncropped image.
-     * 
+     *
      * @return Miyuki
      */
-    
+
     public function crop($width, $height, $x = 0, $y = 0)
     {
         $this->imageBlock -> cropImage($width, $height, $x, $y);
-        
+
         return $this;
     }
-    
-    
-    
-    
+
+
+
+
     /**
      * Resize
      *
      * resize the image.
-     * 
+     *
      * @param int  $width
      * @param int  $height
      * @param bool $aspectRatio    Set true to keep the aspect ratio.
      * @param bool $force          Set true to force to resize the image.
-     * 
+     *
      * @return Miyuki
      */
-    
+
     public function resize($width, $height, $aspectRatio = true, $force = false)
     {
         $imageWidth  = $this->imageSize['width'];
         $imageHeight = $this->imageSize['height'];
-        
+
         /** Resize the image with the fixed aspect ratio if we want */
         if($aspectRatio)
         {
@@ -87,7 +87,7 @@ class Miyuki
             $width  = $ratio['width'];
             $height = $ratio['height'];
         }
-        
+
         if($imageWidth > $width || $imageHeight > $height || $force)
         {
             $this->imageBlock -> resizeImage($width, $height, $this->filter, 1);
@@ -95,96 +95,96 @@ class Miyuki
             /** Sharp the resized image cause the image will be a little bit blurred after resized */
             $this->imageBlock -> unsharpMaskImage(0, 0.5, 1, 0.05);
         }
-        
+
         return $this;
-    }    
-    
-    
-    
-    
+    }
+
+
+
+
     /**
      * Scale
      *
      * Scale the image and IGNORE any filters.
-     * 
+     *
      * @param int  $width
      * @param int  $height
      * @param bool $aspectRatio    Set true to keep the aspect ratio.
      * @param bool $force          Set true to force to scale the image.
-     * 
+     *
      * @return Miyuki
      */
-    
+
     public function scale($width, $height, $aspectRatio = true, $force = false)
     {
         $imageWidth  = $this->imageSize['width'];
         $imageHeight = $this->imageSize['height'];
-        
+
         if($aspectRatio)
         {
             $ratio  = $this->aspectRatio($imageWidth, $imageHeight, $width, $height);
-            
+
             $width  = $ratio['width'];
             $height = $ratio['height'];
         }
-        
-        
+
+
         if($imageWidth > $width || $imageHeight > $height || $force)
             $this->imageBlock -> scaleImage($width, $height);
-        
+
         return $this;
     }
-    
-    
-    
-    
+
+
+
+
     /**
      * Thumbnail
      *
      * More faster than resize with less file size, and remove the exif automantically.
-     * 
+     *
      * @param int  $width
      * @param int  $height
      * @param bool $aspectRatio    Set true to keep the aspect ratio.
-     * 
-     * @return 
+     *
+     * @return
      */
-    
+
     public function thumbnail($width, $height, $aspectRatio = true)
     {
         $imageWidth  = $this->imageSize['width'];
         $imageHeight = $this->imageSize['height'];
-        
+
         if($aspectRatio)
         {
             $ratio  = $this->aspectRatio($imageWidth, $imageHeight, $width, $height);
-            
+
             $width  = $ratio['width'];
             $height = $ratio['height'];
         }
-        
+
         if($imageWidth > $width || $imageHeight > $height)
             $this->imageBlock -> thumbnailImage($width, $height);
-        
+
         return $this;
     }
-    
-    
-    
-    
+
+
+
+
     /**
      * Aspect Ratio
-     * 
+     *
      * Calculate and return the best ratio of the image.
-     * 
+     *
      * @param int $srcWidth
      * @param int $srcHeight
      * @param int $maxWidth
      * @param int $maxHeight
-     * 
+     *
      * @return array
      */
-    
+
     public function aspectRatio($srcWidth, $srcHeight, $maxWidth, $maxHeight)
     {
         /** Calculate the best width and height */
@@ -209,75 +209,92 @@ class Miyuki
             $width  = $srcWidth;
             $height = $srcHeight;
         }
-        
+
         return ['width'  => $width,
                 'height' => $height];
     }
-    
-    
-    
-    
+
+
+
+
     /**
      * Write
-     * 
+     *
      * Save the processed image and return the path.
-     * 
+     *
      * @param string|bool $path     The path with the file name, when false we will save the image
      *                              into a temporary folder.
-     * 
+     *
      * @return string
      */
-    
+
     public function write($path)
     {
-        if($path === false)
-            $path = tempnam(sys_get_temp_dir(), '');
-        
-        $path .= '.' . $this->imageType['extension'];
+        if($path === false || $path === null)
+            $finalPath = tempnam(sys_get_temp_dir(), '') . '.' . $this->imageType['extension'];
+        elseif($path === true)
+            $finalPath = $this->imagePath;
+        else
+            $finalPath = $path;
 
-        $this->imageBlock -> writeImage($path);
+        $this->imageBlock -> writeImage($finalPath);
         $this->imageBlock -> clear();
         //$this->clear();
-        
-        return $path;
+
+        return $finalPath;
     }
-    
-    
-    
-    
+
+
+
+
     /**
      * Set Quality
-     * 
+     *
      * Change the quality of the final image.
-     * 
+     *
      * @param int $quality   The quality value, min: 0.1, max: 1.
-     * 
+     *
      * @return Miyuki
      */
-    
+
     function setQuality($quality)
     {
         /** Convert the float into int for imagick */
-        $this->quality = $quality * 100;
-        
+        $this->quality = $quality * 10;
+        //$this->quality = $quality;
+
         $this->imageBlock -> setImageCompressionQuality($this->quality);
-        
+
         return $this;
     }
-    
-    
-    
-    
+
+
+
+
+    /**
+     * Set Image Compression
+     *
+     */
+
+    function setImageCompression($switch)
+    {
+        $this->imageBlock -> setImageCompression($switch);
+
+        return $this;
+    }
+
+
+
     /**
      * Set Compression
-     * 
+     *
      * Set the image compression.
-     * 
+     *
      * @param string $compression   The name of the compression.
-     * 
+     *
      * @return Miyuki
      */
-    
+
     function setCompression($compression)
     {
         switch($compression)
@@ -285,64 +302,64 @@ class Miyuki
             case 'UNDEFINED':
                 $this->imageBlock -> setImageCompression(imagick::COMPRESSION_UNDEFINED);
                 break;
-            
+
             case 'NO':
                 $this->imageBlock -> setImageCompression(imagick::COMPRESSION_NO);
                 break;
-            
+
             case 'BZIP':
                 $this->imageBlock -> setImageCompression(imagick::COMPRESSION_BZIP);
                 break;
-                
+
             case 'FAX':
                 $this->imageBlock -> setImageCompression(imagick::COMPRESSION_FAX);
                 break;
-            
+
             case 'GROUP4':
                 $this->imageBlock -> setImageCompression(imagick::COMPRESSION_GROUP4);
                 break;
-                
+
             case 'JPEG':
                 $this->imageBlock -> setImageCompression(imagick::COMPRESSION_JPEG);
                 break;
-                
+
             case 'JPEG2000':
                 $this->imageBlock -> setImageCompression(imagick::COMPRESSION_JPEG2000);
                 break;
-                
+
             case 'LOSSLESSJPEG':
                 $this->imageBlock -> setImageCompression(imagick::COMPRESSION_LOSSLESSJPEG);
                 break;
-                
+
             case 'LZW':
                 $this->imageBlock -> setImageCompression(imagick::COMPRESSION_LZW);
                 break;
-                
+
             case 'RLE':
                 $this->imageBlock -> setImageCompression(imagick::COMPRESSION_RLE);
                 break;
-                
+
             case 'ZIP':
                 $this->imageBlock -> setImageCompression(imagick::COMPRESSION_ZIP);
                 break;
         }
-        
+
         return $this;
     }
-    
-    
-    
-    
+
+
+
+
     /**
      * Set Filter
-     * 
+     *
      * Change the filter.
-     * 
+     *
      * @param string $filter   The name of the filter.
      *
      * @return Miyuki
      */
-    
+
     function setFilter($filter)
     {
         switch($filter)
@@ -350,28 +367,28 @@ class Miyuki
             case 'LANCZOS':
                 $this->filter = Imagick::FILTER_LANCZOS;
                 break;
-                
+
             case 'POINT':
                 $this->filter = Imagick::FILTER_POINT;
                 break;
         }
-        
+
         return $this;
     }
-    
-    
-    
-    
+
+
+
+
     /**
      * Set Type
-     * 
+     *
      * Change the type of the final image.
-     * 
+     *
      * @param string $type   The type name.
-     * 
+     *
      * @return Miyuki
      */
-    
+
     function setType($type)
     {
         switch($type)
@@ -386,94 +403,94 @@ class Miyuki
                 $this->imageBlock -> setImageFormat('jpeg');
                 break;
         }
-        
+
         return $this;
     }
-    
-    
-    
-    
+
+
+
+
     /**
      * Check File Size
-     * 
+     *
      * Returns true when the file size is under the limit.
-     * 
+     *
      * @param int $limit   KB.
-     * 
+     *
      * @return bool
      */
-     
+
     function checkFilesize($limit)
     {
         $limit = $limit * 1000;
-        
+
         return filesize($this->imagePath) < $limit;
     }
-    
-    
-    
-    
+
+
+
+
     /**
      * Check Type
-     * 
+     *
      * Returns true when the type of the image is valid.
-     * 
+     *
      * @param string $allowed   A string which contains the allowed types like 'jpg, png'.
      *
      * @return bool
      */
-     
+
     function checkType($allowed)
     {
         $allowed = explode(', ', $allowed);
 
         return in_array($this->imageType['extension'], $allowed);
     }
-    
-    
-    
-    
+
+
+
+
     /**
      * Check Size
-     * 
+     *
      * Returns true if the width and the height of the image are okay :D
-     * 
+     *
      * @param int $width       The max width of the image should be.
      * @param int $height      The max height of the image should be.
      * @param int $minWidth    The min width of the image.
      * @param int $maxHeight   The min height of the image.
-     * 
+     *
      * @return bool
      */
-    
+
     function checkSize($width, $height, $minWidth = 1, $minHeight = 1)
     {
         if($this->imageSize['width']  > $width  || $this->imageSize['width']  < $minWidth ||
            $this->imageSize['height'] > $height || $this->imageSize['height'] < $minHeight)
             return false;
-        
+
         return true;
     }
-    
-    
-    
-    
+
+
+
+
     /**
-     * Get Type 
-     * 
+     * Get Type
+     *
      * Returns the type of the current image block.
-     * 
+     *
      * @return array
      */
-    
+
     function getType()
     {
         $finfo    = finfo_open(FILEINFO_MIME_TYPE);
         $mime     = finfo_file($finfo, $this->imagePath);
         $extesion = '';
-        
+
         finfo_close($finfo);
-        
+
         switch($mime)
         {
             case 'image/png' : $extesion = 'png';  break;
@@ -486,29 +503,29 @@ class Miyuki
             default:
                 $extesion = false;
         }
-        
-        return ['mime'      => $mime, 
+
+        return ['mime'      => $mime,
                 'extension' => $extesion];
     }
-    
-    
-    
-    
+
+
+
+
     /**
      * Get Size
-     * 
+     *
      * Returns the width and the height of the image.
-     * 
+     *
      * @return array
      */
-    
+
     function getSize()
     {
         list($width, $height) = getimagesize($this->imagePath);
-        
+
         return ['width'  => $width,
                 'height' => $height];
     }
- 
+
 }
 ?>
